344860397e9787688313506adfd3d5465bb83617
diegocar448 <diegocar448@hotmail.com>
2023-06-28 11:46:07 -0300

    CartaoConteudo Finalizado

--- src/components/social/CardConteudoVue.vue
+++ src/components/social/CardConteudoVue.vue
@@ -3,28 +3,20 @@
     <div class="card-content">
       <div class="row valign-wrapper">
         <grid-vue tamanho="1">
-          <img src="https://materializecss.com/images/yuna.jpg" alt="" class="circle responsive-img"> <!-- notice the "circle" class -->
+          <img :src="perfil" :alt="nome" class="circle responsive-img"> <!-- notice the "circle" class -->
         </grid-vue>
         <grid-vue tamanho="11">
           <span class="black-text">
-            <strong>Maria Souza</strong> - <small>28/06/23 11:04</small>
+            <strong>{{nome}}</strong> - <small>{{ data }}</small>
           </span>
         </grid-vue>
       </div>
-      <div class="card-image">
-          <img src="https://materializecss.com/images/sample-1.jpg">
-          <span class="card-title">Card Title</span>
-      </div>
-      <div class="card-content">
-        <p>I am a very simple card. I am good at containing small bits of information.
-        I am convenient because I require little markup to use effectively.</p>
-      </div>
+      <slot></slot>
     </div>
     <div class="card-action">
       <p>
         <i class="material-icons">favorite_border</i>
         <i class="material-icons">insert_comment  </i>
-
       </p>
     </div>
   </div>
@@ -34,7 +26,7 @@
 import GridVue from '@/components/layouts/GridVue';
 export default {
   name: 'CardConteudoVue',
-  props:[],
+  props:['perfil', 'nome', 'data'],
   data(){
     return {
 
--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -1,6 +1,13 @@
 <template>
-  <card-conteudo-vue>
-
+  <card-conteudo-vue perfil="https://materializecss.com/images/yuna.jpg" nome="Maria" data="28/06/2023 11:42">
+    <div class="card-image">
+        <img src="https://materializecss.com/images/sample-1.jpg">
+        <span class="card-title">Card Title</span>
+    </div>
+    <div class="card-content">
+      <p>I am a very simple card. I am good at containing small bits of information.
+      I am convenient because I require little markup to use effectively.</p>
+    </div>
   </card-conteudo-vue>
 </template>
 
