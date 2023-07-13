1e5ec9af68e206dd2b4df56cc08ed26266248da3
diegocar448 <diegocar448@hotmail.com>
2023-07-13 14:48:08 -0300

    mode history / router-link

--- src/templates/SiteTemplate.vue
+++ src/templates/SiteTemplate.vue
@@ -2,9 +2,9 @@
   <span>
 
     <header>
-      <nav-bar logo="Social" url="#/" cor="green darken-1">
-        <li><a href="#/">Home</a></li>
-        <li><a href="#/login">Entrar</a></li>
+      <nav-bar logo="Social" url="/" cor="green darken-1">
+        <li><router-link to="/">Home</router-link></li>
+        <li><router-link to="/login">Entrar</router-link></li>
       </nav-bar>
     </header>
 
